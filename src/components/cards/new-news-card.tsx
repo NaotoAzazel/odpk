6d29ca851874578e9c6255fe67e2ea422cbd7b95
Igor <87926186+NaotoAzazel@users.noreply.github.com>
2024-05-27 15:19:54 +0300

    changed style of new-news-card

--- src/components/cards/new-news-card.tsx
+++ src/components/cards/new-news-card.tsx
@@ -1,9 +1,6 @@
 import { Post } from "@/types";
 
 import { Picture } from "@/components/picture";
-import { buttonVariants } from "@/components/ui/button";
-
-import { cn } from "@/lib/utils";
 
 import Link from "next/link";
 
@@ -13,32 +10,25 @@ interface NewsCardProps {
 
 export function NewNewsCard({ post }: NewsCardProps) {
   return (
-    <div className="drop-shadow overflow-hidden rounded-lg bg-background">
-      <div className="flex flex-col">
-        <div className="relative pb-72 flex inset-0">
-          <div className="flex w-full h-full absolute">
-            <Picture
-              src={post.images[0]}
-              className="object-cover inset-0"
-            />
+    <Link href={`/news/${post.id}`}>
+      <div className="shadow hover:shadow-lg transition-shadow duration-400 border overflow-hidden rounded-lg bg-background">
+        <div className="flex flex-col">
+          <div className="relative pb-72 flex inset-0">
+            <div className="flex w-full h-full absolute">
+              <Picture
+                src={post.images[0]}
+                className="object-cover inset-0 border-b rounded-none"
+              />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="p-4 overflow-hidden">
-        <h1 className="overflow-hidden whitespace-nowrap text-ellipsis">
-          {post.title}
-        </h1>
+        <div className="p-4 overflow-hidden">
+          <h1 className="overflow-hidden whitespace-nowrap text-ellipsis font-medium">
+            {post.title}
+          </h1>
+        </div>
       </div>
-
-      <div className="flex pt-1 p-4 w-full items-center">
-        <Link
-          className={cn(buttonVariants({ variant: "outline" }))}
-          href={`/news/${post.id}`}
-        >
-          Переглянути
-        </Link>
-      </div>
-    </div>
+    </Link>
   );
 }
