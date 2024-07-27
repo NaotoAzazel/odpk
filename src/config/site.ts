226267cefffbb722717bcb9ffc3af2ab970c0c5d
Vladislav <danirucakov@gmail.com>
2024-07-27 21:40:01 +0300

    expanded siteConfig

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,17 @@
-export const siteConfig = {
-  name: "ОДПК",
+import { env } from "@/env";
+
+export type SiteConfig = typeof siteConfig;
+
+const links = {
   youtube: "youtube url",
   facebook: "facebook url",
   instagram: "instagram url",
+};
+
+export const siteConfig = {
+  name: "ОДПК",
+  description:
+    "Офіційний сайт Олександрійського політехнічного фахового коледжу",
+  url: env.NEXT_PUBLIC_APP_URL,
+  links,
 };
