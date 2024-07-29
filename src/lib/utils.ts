1faf59f08714cba9309e765a1656c8ca224f3f8b
Vladislav <danirucakov@gmail.com>
2024-07-29 16:42:17 +0300

    refactor: add function to format date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,15 @@ export function cn(...inputs: ClassValue[]) {
 export function absoluteUrl(path: string) {
   return `${env.NEXT_PUBLIC_APP_URL}${path}`;
 }
+
+export function formatDate(
+  date: Date | string | number,
+  options: Intl.DateTimeFormatOptions = {},
+) {
+  return new Intl.DateTimeFormat("uk-UA", {
+    year: options.year ?? "numeric",
+    month: options.month ?? "numeric",
+    day: options.day ?? "numeric",
+    ...options,
+  }).format(new Date(date));
+}
