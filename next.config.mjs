7097c345054607f8d1419bd5377f66f28fa5c556
Vladislav <danirucakov@gmail.com>
2024-08-14 00:55:26 +0300

    added image remote pattern

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,10 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "utfs.io"
+      },
+      {
+        protocol: "http",
+        hostname: "odpk.org.ua"
       }
     ]
   }
