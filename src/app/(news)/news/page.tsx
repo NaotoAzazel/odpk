0530bd0150d658d6a1798616ec409a022f2ddd7a
Vladislav <danirucakov@gmail.com>
2024-08-31 21:26:46 +0300

    added error boundary to news cards

--- src/app/(news)/news/page.tsx
+++ src/app/(news)/news/page.tsx
@@ -1,7 +1,9 @@
 import { Suspense } from "react";
 
+import ErrorBoundary from "@/components/error-boundary";
 import { Header } from "@/components/header";
 import MaxWidthWrapper from "@/components/max-width-wrapper";
+import { NewsCardsErrorContainer } from "@/components/news-cards-error-container";
 import { NewsLoadingContainer } from "@/components/skeletons/news-loading-container";
 
 import { NewsContainer } from "./_components/news-container";
@@ -18,16 +20,18 @@ export default function NewsPage() {
           heading="Поточні новини"
           text="Нижче відображаються всі новини"
         />
-        <Suspense
-          fallback={
-            <NewsLoadingContainer
-              className="w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
-              cardsCount={6}
-            />
-          }
-        >
-          <NewsContainer />
-        </Suspense>
+        <ErrorBoundary fallback={<NewsCardsErrorContainer />}>
+          <Suspense
+            fallback={
+              <NewsLoadingContainer
+                className="w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
+                cardsCount={6}
+              />
+            }
+          >
+            <NewsContainer />
+          </Suspense>
+        </ErrorBoundary>
       </div>
     </MaxWidthWrapper>
   );
