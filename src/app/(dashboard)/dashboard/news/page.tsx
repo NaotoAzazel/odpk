47519bf63b00e9be05cb45270fd8b9545fd65bee
Vladislav <danirucakov@gmail.com>
2024-09-05 18:53:28 +0300

    moved dashboard-shell to components

--- src/app/(dashboard)/dashboard/news/page.tsx
+++ src/app/(dashboard)/dashboard/news/page.tsx
@@ -1,16 +1,14 @@
-import { DataTableSkeleton } from "@/components/data-table/data-table-skeleton";
-
-import DashboardShell from "./_components/dashboard-shell";
-import { NewsTable } from "./_components/news-table";
+import { Suspense } from "react";
+import { Metadata } from "next";
 
 import { getNews } from "@/lib/actions/news";
-
+import DashboardShell from "@/components/dashboard-shell";
+import { DataTableSkeleton } from "@/components/data-table/data-table-skeleton";
 import ErrorBoundary from "@/components/error-boundary";
 import { Header } from "@/components/header";
 import { NewsCardsErrorContainer } from "@/components/news-cards-error-container";
-import { Suspense } from "react";
 
-import { Metadata } from "next";
+import { NewsTable } from "./_components/news-table";
 
 export const metadata: Metadata = {
   title: "Новини",
