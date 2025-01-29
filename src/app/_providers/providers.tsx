22992ff7075ecf06b0df9ca726e6f9e59c3446b6
Vladislav <danirucakov@gmail.com>
2025-01-29 17:34:03 +0200

    added tanstack query

--- src/app/_providers/providers.tsx
+++ src/app/_providers/providers.tsx
@@ -3,6 +3,7 @@
 import { SessionProvider } from "next-auth/react";
 
 import { CheckSession } from "./check-session";
+import { TanstackProvider } from "./tanstack-provider";
 
 interface ProvidersProps {
   children: React.ReactNode;
@@ -11,7 +12,9 @@ interface ProvidersProps {
 export function Providers({ children }: ProvidersProps) {
   return (
     <SessionProvider>
-      <CheckSession>{children}</CheckSession>
+      <TanstackProvider>
+        <CheckSession>{children}</CheckSession>
+      </TanstackProvider>
     </SessionProvider>
   );
 }
